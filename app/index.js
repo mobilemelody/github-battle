4c0514b73c14cfd644c9d9124e9a4f2e188f171f
mobilemelody <melody.reebs@gmail.com>
2017-06-11 18:54:26 -0700

    setup basic components

--- app/index.js
+++ app/index.js
@@ -1,16 +1,8 @@
-var React = requie('react');
+var React = require('react');
 var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
 require('./index.css');
-
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                Hello World!
-            </div>
-        );
-    }
-}
+var App = require('./components/App');
 
 ReactDOM.render(
     <App />,
